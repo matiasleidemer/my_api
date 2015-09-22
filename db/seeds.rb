ff7db85d8b65b784a8168a1e922495fb8a293457
Matias Leidemer <matiasleidemer@gmail.com>
2015-09-21 22:03:26 -0300

    feature(Seed): add seed file to populate develop database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.create!(email: 'admin@trycatch.us', password: '123change')
+user  = User.create!(email: 'user@trycatch.us' , password: 'change123')
+
+4.times do
+  article = Article.create!(
+    title: Faker::Hacker.say_something_smart,
+    body: Faker::Lorem.paragraph(7),
+    author: [admin, user].sample
+  )
+
+  5.times do
+   Comment.create!(
+      author: Faker::Name.name,
+      body: Faker::Lorem.paragraph(7),
+      article: article
+    )
+  end
+end
