0888e760fce94a7cb8f2095a4af6acae32e4a767
Matias Leidemer <matiasleidemer@gmail.com>
2015-09-22 23:19:27 -0300

    feature(Comment): add Comments controller
    Controller is a nested resource from Article

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,36 @@
+class Api::V1::CommentsController < ApiController
+  load_and_authorize_resource :article
+  load_and_authorize_resource :comment, through: :article
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      render :show, status: :created
+    else
+      render json: { errors: @comment.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @comment.update_attributes(comment_params)
+      render :show, status: :ok
+    else
+      render json: { errors: @comment.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @comment.destroy
+      head :no_content
+    else
+      head :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:author, :title, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api, constraints: { format: 'json' } do
     namespace :v1 do
-      resources :articles
+      resources :articles do
+        resources :comments
+      end
     end
   end
 end
