5e325154911dbdee56e05b727f51e836da719176
Matias Leidemer <matiasleidemer@gmail.com>
2015-09-22 22:17:29 -0300

    fix(Authorization): bypass access if it doesn't find a valid user
    If there's no user with the credentials (or in case they are missing)
    the user will be treated as a guest.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,13 @@
 class ApiController < ApplicationController
-  acts_as_token_authentication_handler_for User
+  acts_as_token_authentication_handler_for User, fallback: :none
 
   load_and_authorize_resource
 
   before_action :set_default_response_format
+
+  rescue_from CanCan::AccessDenied do |exception|
+    render json: exception.message, status: :unauthorized
+  end
 
   private
 
